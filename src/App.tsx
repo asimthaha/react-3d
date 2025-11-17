eec3829606ad729f360a62773ebc81413fab5dd9
Asim Thaha <iamasimthaha@gmail.com>
2025-11-17 20:46:40 +0530

    deployment corrections

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/react-3d">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
